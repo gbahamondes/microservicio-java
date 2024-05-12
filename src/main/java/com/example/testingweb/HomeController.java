0f746cc133dde746cc3e4b924974e11f538e0d0e
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-05-12 08:33:00 -0400

    KAN-1 fixing the expect on the test

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Hello, I'm Giovanni Bahamondes!!   ";
+		return "Hello, I'm Giovanni Bahamondes!!";
 	}
 
 }
