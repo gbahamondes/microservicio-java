fd8e842eba04d1df6b4ef6488996891902f3cb80
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-05-14 23:18:27 -0400

    KAN-1 added quality gate

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -1,5 +1,5 @@
 package com.example.testingweb;
-
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@GetMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 	public @ResponseBody String greeting() {
-		return "Hello, I'm Giovanni Bahamondes!!   ";
+		return "Hello, I'm Giovanni Bahamondes!!";
 	}
 
 }
