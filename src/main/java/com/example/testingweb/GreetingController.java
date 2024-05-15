51ebb0e0997cb0143e13435c2300bbafe3134b5c
Giovanni Bahamondes <giovannibahamondes@gmail.com>
2024-05-14 23:24:37 -0400

    KAN-1 added quality gate

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -3,6 +3,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GreetingController {
